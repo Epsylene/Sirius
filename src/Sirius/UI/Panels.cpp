90a841efad5601a13cff5443d83bbcd989a69164
Epsylene <epsylene@gmail.com>
2021-10-28 20:56:26 +0200

    The render mode checkbox is now a toggle button.

--- src/Sirius/UI/Panels.cpp
+++ src/Sirius/UI/Panels.cpp
@@ -33,6 +33,28 @@ namespace Sirius
 
     }
 
+    void toggleButton(const char* str_id, bool* v)
+    {
+        ImVec2 p = ImGui::GetCursorScreenPos();
+        ImDrawList* draw_list = ImGui::GetWindowDrawList();
+
+        float height = ImGui::GetFrameHeight();
+        float width = height * 1.55f;
+        float radius = height * 0.50f;
+
+        if (ImGui::InvisibleButton(str_id, ImVec2(width, height)))
+            *v = !*v;
+        ImU32 col_bg;
+        if (ImGui::IsItemHovered())
+            col_bg = *v ? IM_COL32(145+20, 211, 68+20, 255) : IM_COL32(218-20, 218-20, 218-20, 255);
+        else
+            col_bg = *v ? IM_COL32(145, 211, 68, 255) : IM_COL32(218, 218, 218, 255);
+
+        draw_list->AddRectFilled(p, ImVec2(p.x + width, p.y + height), col_bg, height * 0.5f);
+        draw_list->AddCircleFilled(ImVec2(*v ? (p.x + width - radius) : (p.x + radius), p.y + radius), radius - 1.5f, IM_COL32(255, 255, 255, 255));
+        draw_list->AddText(ImVec2(p.x + width + 10, p.y + height / 5.f), col_bg, str_id);
+    }
+
     void PropertiesPanel::render()
     {
         ImGui::Begin("Properties");
@@ -97,7 +119,7 @@ namespace Sirius
         {
             ImGui::ColorEdit3("Scene background color", &Scene::properties.background.r);
             ImGui::Checkbox("Wireframe mode", &Scene::properties.wireframe);
-            ImGui::Checkbox("2D mode", &Scene::properties.render2D);
+            toggleButton("2D mode", &Scene::properties.render2D);
 
             ImGui::Combo("Postprocessing", &selectedFlag, &ppFlagsStrs[0], ppFlagsStrs.size());
             Scene::properties.ppFlag = PostProcessingFlags(selectedFlag);
